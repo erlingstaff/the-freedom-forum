b06b8762be9a997e49d496900eeaa4e1462ec710
ErlingHW <erlingstaff@gmail.com>
2022-04-28 18:59:11 +0200

    Initial app commit

---
+++ android-prototype/app/src/main/java/com/example/thefreedomforum/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thefreedomforum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()                          //hide the title bar
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ testbl.py
@@ -0,0 +1,9 @@
+import bluetooth
+
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("Found {} devices.".format(len(nearby_devices)))
+
+for addr, name in nearby_devices:
+    print("  {} - {}".format(addr, name))
+
+bluetooth.find_service()
