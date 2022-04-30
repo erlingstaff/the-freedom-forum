969166b98c85064f07f7ad60bb85dda83a492ca9
ErlingHW <erlingstaff@gmail.com>
2022-05-01 00:07:53 +0200

    new functions added inheader file

--- lora-relay-prototype/LoRa/LoRa.h
+++ lora-relay-prototype/LoRa/LoRa.h
@@ -1,11 +1,7 @@
 void setup();
-
 void loRaConfig();
-
 void loop();
-
 void checkMessage(String message);
-
 void sendMessageOverLoRa(String message);
-
 void sendMessageOverBLE(String message);
+void checkIfRelayMessage(String message);
