b22adb638bacff3f89c5cbbc2517a2d5b78e34b7
ErlingHW <erlingstaff@gmail.com>
2022-05-02 22:52:51 +0200

    ⚡bluetooth updates. sending messages completed, GATT server not done yet

--- android-prototype/app/src/main/java/com/example/thefreedomforum/MainActivity.kt
+++ android-prototype/app/src/main/java/com/example/thefreedomforum/MainActivity.kt
@@ -2,25 +2,25 @@ package com.example.thefreedomforum
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
+import android.app.AlertDialog
+import android.bluetooth.*
+import android.bluetooth.le.*
+import android.content.DialogInterface
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
 import android.util.Log
 import android.view.Gravity
-import android.view.View
-import android.view.WindowManager
-import android.widget.*
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.thefreedomforum.adapters.ItemAdapter
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,19 +28,158 @@ class MainActivity : AppCompatActivity() {
     private var itms = ArrayList<ArrayList<String>>()
     private lateinit var adapter: ItemAdapter
     private lateinit var recyclerView: RecyclerView
+    private val PERMISSION_REQUEST_FINE_LOCATION = 1
+    private val PERMISSION_REQUEST_BACKGROUND_LOCATION = 2
+
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()                          //hide the title bar
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                        builder.setTitle("This app needs background location access")
+                        builder.setMessage("Please grant location access so this app can detect beacons in the background.")
+                        builder.setPositiveButton(android.R.string.ok, null)
+                        builder.setOnDismissListener(DialogInterface.OnDismissListener {
+                            requestPermissions(
+                                arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                                PERMISSION_REQUEST_BACKGROUND_LOCATION
+                            )
+                        })
+                        builder.show()
+                    } else if (1 == 2) {
+                        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                        builder.setTitle("Functionality limited")
+                        builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.")
+                        builder.setPositiveButton(android.R.string.ok, null)
+                        builder.setOnDismissListener(DialogInterface.OnDismissListener { })
+                        builder.show()
+                    }
+                }
+            } else {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    requestPermissions(
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        ),
+                        PERMISSION_REQUEST_FINE_LOCATION
+                    )
+                } else {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location access to this app.")
+                    builder.setPositiveButton(android.R.string.ok, null)
+                    builder.setOnDismissListener(DialogInterface.OnDismissListener { })
+                    builder.show()
+                }
+            }
+        }
+
+        val uuid = UUID.fromString("BEB5483E-36E1-4688-B7F5-EA07361B26A8")
+        val devicename = "freedomforum"
         val bluetoothManager = applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
         val bluetoothAdapter = bluetoothManager.adapter
+
+        val newuuid = UUID.fromString("4fafc201-1fb5-459e-8fcc-c5c9c331914b")
+
+        val bleAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser()
+
+
+
+
+
 
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, 1)
         }
+        val settings = ScanSettings.Builder().setScanMode(ScanSettings.CALLBACK_TYPE_ALL_MATCHES).build()
+
+
+        val devices = mutableListOf<BluetoothDevice>()
+        val bluetoothGattService = bluetoothAdapter.bluetoothLeScanner.startScan(object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                val device = result.device
+                val deviceName = device.name
+                if (deviceName == "freedomforum"){
+                    device.connectGatt(this@MainActivity, false, object : BluetoothGattCallback() {
+                        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+                            super.onConnectionStateChange(gatt, status, newState)
+                            if (newState == BluetoothProfile.STATE_CONNECTED){
+                                gatt?.discoverServices()
+                            }
+                        }
+
+                        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                            super.onServicesDiscovered(gatt, status)
+                            if (status == BluetoothGatt.GATT_SUCCESS){
+                                val srv = gatt?.services
+                                for (service in srv!!){
+                                    val characteristics = service.characteristics
+                                    for (characteristic in characteristics){
+                                        if (characteristic.uuid == uuid){
+                                            Log.d("herher", "Found characteristic "+characteristic.uuid)
+                                            characteristic.setValue("hei hei");
+                                            characteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+                                            gatt.writeCharacteristic(characteristic);
+                                            gatt.readCharacteristic(characteristic);
+                                            gatt.disconnect()
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+
+                        override fun onCharacteristicRead(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
+                            super.onCharacteristicRead(gatt, characteristic, status)
+                            val value = characteristic!!.value
+                            Log.d("herher", "Read value "+value.toString())
+
+                        }
+                    })
+                }
+                val deviceAddress = device.address
+                val deviceRssi = result.rssi
+                val deviceUUID = result.scanRecord?.serviceUuids
+                val deviceData = ArrayList<String>()
+                deviceData.add(deviceName)
+                deviceData.add(deviceAddress)
+                deviceData.add(deviceRssi.toString())
+                deviceData.add(deviceUUID.toString())
+                Log.d("Devices", deviceData.toString())
+                devices.add(device)
+                if (devices.size > 5){
+                    Log.d("innom", "innom")
+                    bluetoothAdapter.bluetoothLeScanner.stopScan(object : ScanCallback(){
+                        override fun onScanFailed(errorCode: Int) {
+                            super.onScanFailed(errorCode)
+                            Log.d("Devices", "Scan failed")
+                        }
+
+                        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+                            super.onScanResult(callbackType, result)
+                            Log.d("Devices", "Scan result")
+                        }
+                    })
+                    devices.clear()
+                }
+            }
+        })
+
+        bluetoothAdapter.bluetoothLeAdvertiser
 
         val settingsbtn = findViewById<ImageView>(R.id.settingsbtn)
         settingsbtn.setOnClickListener{
