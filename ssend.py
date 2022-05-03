b5311bde2bbccdb0843900c7f383b7eb98d8b44e
ErlingHW <erlingstaff@gmail.com>
2022-05-03 23:02:47 +0200

    📁 folder for python-scripts used for testing and verifying network stability

--- ssend.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-PC2_IP = '0.0.0.0' # IP address is not valid, it is just for example. 
-UDP_PORT = 5005
-
-while True:
-    message = input("What do want to say?\n")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(message, (PC2_IP, UDP_PORT))
